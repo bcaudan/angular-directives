0596bafc08fa38a6da2a15d7c648e323554d53bb
Bastien Caudan <bastien.caudan@gmail.com>
2013-05-09 12:02:12 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-angular-directive
-=================
+angular-directives
+==================
---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('angular-directives', []);
+
+angular.module('angular-directives', []).controller('home', ['$scope', function ($scope) {
+    $scope.test = "test";
+}]);
