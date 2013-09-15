b9960f5dcfee562765b2e931c362df3850962d9a
Bastien Caudan <bastien.caudan@gmail.com>
2013-09-15 13:03:51 +0200

    clean main

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,7 @@ angular.module('angularDirectivesApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/main.html'
       })
       .otherwise({
         redirectTo: '/'
