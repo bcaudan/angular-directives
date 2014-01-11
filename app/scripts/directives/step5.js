eb033dea76385cfc4e1839dc30861bf6ea8e520b
Bastien Caudan <bastien.caudan@gmail.com>
2014-01-11 11:59:58 +0100

    Make jshint happy

--- app/scripts/directives/step5.js
+++ app/scripts/directives/step5.js
@@ -1,55 +1,57 @@
 'use strict';
 
 angular.module('angularDirectivesApp')
-  .directive('tabsS5', function() {
+  .directive('tabsS5', function () {
     return {
-        transclude: true,
-        template: '<ul class="nav nav-tabs">' +
-            '<li ng-repeat="tab in tabs" ' +
-            '    ng-class="{active: isActive(tab)}" ' +
-            '    ng-click="selectTab(tab)">' +
-            '  <a href="">{{ tab.title }}</a>' +
-            '</li>' +
-            '</ul>' +
-            '<div class="tab-content" ng-transclude></div>',
-        require: 'tabs',
-        controller: ['$scope', function(scope) {
-            var tabs = [];
-            var activeTab;
+      transclude: true,
+      template: '<ul class="nav nav-tabs">' +
+        '<li ng-repeat="tab in tabs" ' +
+        '    ng-class="{active: isActive(tab)}" ' +
+        '    ng-click="selectTab(tab)">' +
+        '  <a href="">{{ tab.title }}</a>' +
+        '</li>' +
+        '</ul>' +
+        '<div class="tab-content" ng-transclude></div>',
+      require: 'tabs',
+      controller: ['$scope', function (scope) {
+        var tabs = [];
+        var activeTab;
 
-            this.isActive = function (tab) {
-                return tab == activeTab;
-            };
+        this.isActive = function (tab) {
+          return tab === activeTab;
+        };
 
-            this.selectTab = function(tab) {
-                activeTab = tab;
-            };
+        this.selectTab = function (tab) {
+          activeTab = tab;
+        };
 
-            this.registerTab = function (title, isActive) {
-                var tab = { title: title };
-                tabs.push(tab);
-                if (isActive) activeTab = tab;
-                return tab;
-            };
+        this.registerTab = function (title, isActive) {
+          var tab = { title: title };
+          tabs.push(tab);
+          if (isActive) {
+            activeTab = tab;
+          }
+          return tab;
+        };
 
-            scope.tabs = tabs;
-            scope.isActive = this.isActive;
-            scope.selectTab = this.selectTab;
-        }]
+        scope.tabs = tabs;
+        scope.isActive = this.isActive;
+        scope.selectTab = this.selectTab;
+      }]
     };
   })
-  .directive('tabS5', function() {
+  .directive('tabS5', function () {
     return {
-        transclude: true,
-        replace: true,
-        require: '^tabsS5',
-        scope: true,
-        template: '<div class="tab-pane" ng-class="{active: isActive()}" ng-transclude></div>',
-        link: function (scope, element, attrs, tabsController) {
-            var tab = tabsController.registerTab(attrs.title, attrs.active);
-            scope.isActive = function () {
-                return tabsController.isActive(tab);
-            }
-        }
+      transclude: true,
+      replace: true,
+      require: '^tabsS5',
+      scope: true,
+      template: '<div class="tab-pane" ng-class="{active: isActive()}" ng-transclude></div>',
+      link: function (scope, element, attrs, tabsController) {
+        var tab = tabsController.registerTab(attrs.title, attrs.active);
+        scope.isActive = function () {
+          return tabsController.isActive(tab);
+        };
+      }
     };
   });
